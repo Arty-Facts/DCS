1d525720de5fb014c81b45d7b375bbebabb42294
Arturas Aleksandrauskas <arturas@aleksandraus.se>
2024-02-13 16:10:49 +0100

    Update base-packages.sh

--- environment/base-packages.sh
+++ environment/base-packages.sh
@@ -3,14 +3,18 @@
 # Install base packages
 apt update
 apt install -y --no-install-recommends --fix-missing\
-    python3.10\
-    python3.10-venv\
-    python3.10-dev\
-    software-properties-common\
-    vim\
+    build-essential\
+    default-libmysqlclient-dev\
     emacs\
     git\
     libgl1\
+    openssh-client\
     pciutils\
+    pkg-config\
+    python3.10-dev\
+    python3.10-venv\
+    python3.10\
+    software-properties-common\
     sudo\
-    openssh-client
+    unzip\
+    vim
